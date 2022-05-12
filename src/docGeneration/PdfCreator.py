e8c6dd0685be098221c0b1969a2b079e7479c870
unknown <quentin.clodion@gmail.com>
2022-05-12 20:02:32 +0200

    start PDF

---
+++ src/docGeneration/PdfCreator.py
@@ -0,0 +1,15 @@
+from fpdf import FPDF
+
+
+class PDF(FPDF):
+    def header(self):
+        #self.image('logo_pb.png', 10, 8, 33)
+        self.set_font('Arial', 'B', 15)
+        self.cell(80)
+        self.cell(30, 10, 'Title', 1, 0, 'C')
+        self.ln(20)
+
+    def footer(self):
+        self.set_y(-15)
+        self.set_font('Arial', 'I', 8)
+        self.cell(0, 10, 'Page ' + str(self.page_no()) + '/{nb}', 0, 0, 'C')
--- src/docGeneration/main.py
+++ src/docGeneration/main.py
@@ -1,7 +1,15 @@
 from src.utils.function import *
+from src.docGeneration.PdfCreator import PDF
 
 if __name__ == '__main__':
     code = getFunctFromFile(os.path.dirname(__file__))
     codeArray = createCodeAndSummarize(code)
-    # print(codeArray)
+    print(codeArray)
+    pdf = PDF()
+    pdf.alias_nb_pages()
+    pdf.add_page()
+    pdf.set_font('Times', '', 12)
+    for i in range(1, 41):
+        pdf.cell(0, 10, 'Printing line number ' + str(i), 0, 1)
+    pdf.output('output/tuto2.pdf', 'F')
 
