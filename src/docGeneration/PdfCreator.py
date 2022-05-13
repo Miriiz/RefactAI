ea630d0795d005627d11c6723386cbe6c966e166
unknown <quentin.clodion@gmail.com>
2022-05-13 10:14:15 +0200

    pdf update

--- src/docGeneration/PdfCreator.py
+++ src/docGeneration/PdfCreator.py
@@ -6,13 +6,21 @@ class PDF(FPDF):
         #self.image('logo_pb.png', 10, 8, 33)
         self.set_font('Arial', 'B', 15)
         self.cell(80)
-        self.cell(30, 10, 'Title', 1, 0, 'C')
+        self.cell(45, 10, 'Documentation', 1, 0, 'C')
         self.ln(20)
 
     def footer(self):
         self.set_y(-15)
         self.set_font('Arial', 'I', 8)
         self.cell(0, 10, 'Page ' + str(self.page_no()) + '/{nb}', 0, 0, 'C')
+
+    def add_function(self, func):
+        func_lines = func[0].split("\n")
+        for func_line in func_lines:
+            if len(func_line) != 0:
+                self.cell(0, 10, func_line, 0, 1)
+        self.cell(0, 10, "-> " + func[1][0]['summary_text'], 0, 1)
+        self.ln()
 
     def save(self, filename):
         self.output('output/' + filename + '.pdf', 'F')
--- src/docGeneration/main.py
+++ src/docGeneration/main.py
@@ -4,14 +4,13 @@ from src.docGeneration.PdfCreator import PDF
 if __name__ == '__main__':
     code = getFunctFromFile(os.path.dirname(__file__))
     codeArray = createCodeAndSummarize(code)
-
     print(codeArray)
+
     pdf = PDF()
     pdf.alias_nb_pages()
     pdf.add_page()
     pdf.set_font('Times', '', 12)
     for func in codeArray:
-        pdf.cell(0, 10, func[0], 0, 1)
-        pdf.cell(0, 10, func[1][0]['summary_text'], 0, 1)
+        pdf.add_function(func)
     pdf.save("test")
 
