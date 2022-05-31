8782c141b7395c9b47743083a954b68f54d804ee
Mirii <marvinmiri@hotmail.fr>
2022-05-31 19:30:19 +0200

    Add with iterator need to add Iterator and Class_w, Train_size, val_size

--- src/codeCorrection/Model.py
+++ src/codeCorrection/Model.py
@@ -4,7 +4,9 @@ from tensorflow.keras import Model, Sequential
 from tensorflow.python.keras.layers import Flatten
 from ModelParam import *
 
-
+train_size = 0
+val_size = 0
+class_w = 0
 def create_base_model(add_custom_layers_func) -> Model:
     m = Sequential()
     add_custom_layers_func(m)
@@ -28,3 +30,15 @@ def add_mlp_layers(model):
         model.add(tf.keras.layers.Dense(2048, activation=tf.keras.activations.linear))
         model.add(tf.keras.layers.BatchNormalization())
         model.add(tf.keras.layers.Activation(activation=tf.keras.activations.tanh))
+
+
+def train_model(m: Model, x_iterator, y_iterator):
+    train = m.fit(
+        x_iterator,
+        validation_data=y_iterator,
+        steps_per_epoch=train_size // batch_size,
+        validation_steps=val_size // batch_size,
+        epochs=epch,
+        class_weight=class_w
+    )
+    return train
